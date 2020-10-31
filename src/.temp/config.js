dbaeff598bfee1aa8b7e198307236af99d327ee1
zvekov <mail@zvekov.com>
2020-10-31 17:17:37 +0300

    +

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Yury Zvekov",
-  "siteUrl": "https://zvekov.com",
-  "version": "0.7.21",
-  "catchLinks": true
-}
--- src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1604078109039
