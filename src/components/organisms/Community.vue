8e0c03a48860865eca6829f9805f0f3b380682d8
zvekov <mail@zvekov.com>
2021-06-21 20:24:02 +0300

    +

--- src/components/organisms/Community.vue
+++ src/components/organisms/Community.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="pt-8">
+  <div class="pt-12">
     <h2 class="text-2xl font-bold">Community</h2>
+    <p>Look, read, teach, be friends.</p>
+    <ul class="pt-2">
+      <li>
+        <a href="https://github.com/zvekov">
+          <Github />
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
+<script>
+import Github from "@/components/atoms/icons/Github";
+export default {
+  components: {
+    Github,
+  },
+};
+</script>
+<style lang="postcss" scoped>
+svg {
+  @apply transform duration-300 transition-all;
+  &:hover {
+    @apply scale-125;
+  }
+}
+</style>
