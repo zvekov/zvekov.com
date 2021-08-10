31b19953f124f78ddb355999f8214701f1463cb0
zvekov <mail@zvekov.com>
2021-08-11 00:26:28 +0300

    add wakatime icon

--- src/components/organisms/Community.vue
+++ src/components/organisms/Community.vue
@@ -2,10 +2,15 @@
   <div class="pt-12">
     <h2 class="text-2xl font-bold">Community</h2>
     <p>Look, read, teach, be friends.</p>
-    <ul class="pt-2">
-      <li>
-        <a href="https://github.com/zvekov">
+    <ul class="pt-2 flex items-center">
+      <li class="pr-6">
+        <a href="https://github.com/zvekov" target="_blank">
           <Github />
+        </a>
+      </li>
+      <li class="pr-6">
+        <a href="https://wakatime.com/@zvekov" target="_blank">
+          <WakaTime width="34" height="34" />
         </a>
       </li>
     </ul>
@@ -13,9 +18,11 @@
 </template>
 <script>
 import Github from "@/components/atoms/icons/Github";
+import WakaTime from "@/components/atoms/icons/WakaTime";
 export default {
   components: {
     Github,
+    WakaTime
   },
 };
 </script>
